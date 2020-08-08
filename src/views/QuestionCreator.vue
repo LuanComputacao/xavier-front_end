807e90d997fc2620d50712a2cc168c02cc046a43
LuanComputacao <eng.luanroger@gmail.com>
2020-08-08 17:18:34 -0300

    Preparing question form to create multiple choices questions

--- src/views/QuestionCreator.vue
+++ src/views/QuestionCreator.vue
@@ -6,6 +6,7 @@
         <h2>Formul&aacute;rio</h2>
       </div>
       <form action="" method="POST">
+        <hr/>
         <div>
           <label for="js-question-wording">Enunciado da questão</label><br/>
           <textarea
@@ -17,22 +18,39 @@
           >
           </textarea>
         </div>
-
+        <hr/>
         <div>
           <div>Qual o tipo da questão?</div>
-          <div v-for="(option, i) in questionsTypes" :key="i">
-            {{option.code}}
+          <div v-for="(type, i) in questionsTypes" :key="i">
             <input type="radio"
               :id="'question-type-' + i"
-              :value="option.id"
-              @change="updateAnswer"
-              v-model="picked"
+              :value="type.code"
+              v-model="question_type"
               >
-            <label for="one">{{option.text}}</label>
+            <label :for="'question-type-' + i">{{type.label}}</label>
+          </div>
+        </div>
+        <div v-if="questionsTypes.OPEN.code !== question_type">
+          <hr/>
+          <label for="question-number-of-option">Número de opções da questão: </label>
+          <input
+            v-model="amountOfOptions"
+            id="question-number-of-option"
+            type="number"
+            value="2"
+            max="5"
+            min="2">
+        </div>
+        <hr/>
+        <div v-if="questionsTypes.OPEN.code !== question_type">
+          <h3>Opções</h3>
+          <div v-for="(i, k) in amountOfOptionsComp" :key="k">
+            <input type="text">
           </div>
         </div>
       </form>
     </div>
+    <hr/><hr/>
     <div>
       <div>
         <h2>Preview de Quest&atilde;o</h2>
@@ -62,16 +80,30 @@ export default {
       type: 0,
       wording: '',
       items: [],
-      question_id: ''
+      question_id: '',
+      question_type: 0,
+      amountOfOptions: 1
     }
+  },
+
+  mounted () {
+    this.question_type = this.questionsTypes.OPEN.code
   },
 
   computed: {
     question () {
       return {
         order: 1,
-        wording: this.wording
+        wording: this.wording,
+        type: this.question_type
       }
+    },
+    amountOfOptionsComp () {
+      const a = []
+      for (let i = 1; i <= this.amountOfOptions; i++) {
+        a.push(i)
+      }
+      return a
     },
     ...mapState({
       questionsTypes: state => state.questions.types
